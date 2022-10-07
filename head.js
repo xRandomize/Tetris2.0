188b1dcd7a741e048cdb2ff11c4d65d9f0710f0b
Markus <markus05@hotmail.se>
2022-10-08 01:36:54 +0200

    I have no fucking idea bro

--- head.js
+++ head.js
@@ -1,1 +1,8 @@
+//Build the game map using arrays
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const cols = 10;
+const row = 20;
+const block_size = 30;
+
 
--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press+Start+2P">
+    <title>Tetris</title>
+</head>
+<body>
+    <canvas id="canvas"></canvas>
 
+    <h1>TETRIS<h1>
+    <p>SCORE: <span id="score">0</span><p>
+    <p>LEVEL: <span id="level">0</span><p>
+
+    <script src="head.js"></script>
+</body>
+</html>
